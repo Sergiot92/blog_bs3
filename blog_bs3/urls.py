2c5280b51765aa01e34165bcdb3ed2a91aae2e09
sergio.fitlife@gmail.com <sergio.fitlife@gmail.com>
2013-07-30 19:29:19 -0500

    User sign up

--- blog_bs3/urls.py
+++ blog_bs3/urls.py
@@ -5,6 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'post.views.home', name='home'),
+    url(r'^sign_in', 'post.views.sign_up', name='sign_up'),
     url(r'^log_out', 'post.views.log_out', name='log_out'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	url(r'^admin/', include(admin.site.urls)),
--- post/views.py
+++ post/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponseRedirect
 from post.models import Post
@@ -17,7 +19,29 @@ def home(request):
 		posts = paginator.page(paginator.num_pages)
 	except Exception:
 		posts = paginator.page(1)
-	return render_to_response('home.html',{'posts':posts}, context_instance=RequestContext(request))
+	return render_to_response('home.html', {'posts':posts}, context_instance=RequestContext(request))
+
+def sign_up(request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		passwordc = request.POST['passwordc']
+		if sorted(password) == sorted(passwordc):
+			if User.objects.filter(username=username).count()==0:
+				user = User.objects.create(username=username)
+				user.set_password(password)
+				user.save()
+				msgOk = 'User created.'
+				access = authenticate(username=username, password=password)
+				login(request, access)
+				return render_to_response('sign_up.html', {'msgOk':msgOk}, context_instance=RequestContext(request))
+			else:
+				msgError = 'Username invalid.'
+				return render_to_response('sign_up.html', {'msgError':msgError}, context_instance=RequestContext(request))
+		else:
+			msgError = 'Password confirmation invalid.'
+			return render_to_response('sign_up.html', {'msgError':msgError}, context_instance=RequestContext(request))
+	return render_to_response('sign_up.html', context_instance=RequestContext(request))
 
 def log_out(request):
 	logout(request)
